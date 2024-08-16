28bf13c9ad6bc88925c1c85575e6cd3dc7b498f1
Dolica Akello-Egwel <d.akelloegwel@lcc.arts.ac.uk>
2024-08-16 15:28:18 +0100

    getting return from ask question making the window full screen

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ import pygame
 from ask_question import ask_question
 from traffic_counter import TrafficCounter
 
-models = ["dolphin-phi"]
-
+MODEL = "dolphin-phi"
 traffic_counter = TrafficCounter()
 
-
 pygame.init()
-window = pygame.display.set_mode((500, 200))
+window = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+pygame.display.set_caption("Trafficmancy")
 clock = pygame.time.Clock()
 
-font = pygame.font.SysFont(None, 50)
-text = ""
+font = pygame.font.SysFont(None, 40)
+response_text = ""
+question_text = ""
 input_active = True
 
 run = True
@@ -21,22 +21,22 @@ while run:
     clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False
+            break
         elif event.type == pygame.MOUSEBUTTONDOWN:
             input_active = True
-            text = ""
+            question_text = ""
         elif event.type == pygame.KEYDOWN and input_active:
             if event.key == pygame.K_RETURN:
                 counts = traffic_counter.test()
-                ask_question(models[0], text, counts)
-                text = ""
+                response_text = ask_question(MODEL, question_text, counts)
+                question_text = ""
             elif event.key == pygame.K_BACKSPACE:
-                text = text[:-1]
+                question_text = question_text[:-1]
             else:
-                text += event.unicode
+                question_text += event.unicode
 
         window.fill(0)
-        text_surf = font.render(text, True, (255, 0, 0))
+        text_surf = font.render(question_text, True, (255, 255, 255))
         window.blit(text_surf, text_surf.get_rect(center=window.get_rect().center))
         pygame.display.flip()
 
