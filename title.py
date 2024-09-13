edc23095a20dab412b2b4cf939df56f2f83639f4
Dolica Akello-Egwel <d.akelloegwel@lcc.arts.ac.uk>
2024-09-13 11:44:49 +0100

    removing box from title

--- title.py
+++ title.py
@@ -5,4 +5,4 @@ f = Figlet(font='doom')
 
 with ptg.WindowManager() as manager:
     manager.layout.add_slot("Header")
-    manager.add(ptg.Window(f.renderText('trafficmancy')))
+    manager.add(ptg.Window(f.renderText('trafficmancy'), box="EMPTY"))
