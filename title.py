8248379bb52445acaef7db868bb9932b317f7d04
Dolica Akello-Egwel <d.akelloegwel@lcc.arts.ac.uk>
2024-09-13 11:35:23 +0100

    found a font that works

--- title.py
+++ title.py
@@ -1,5 +1,8 @@
 import pytermgui as ptg
+from pyfiglet import Figlet
+
+f = Figlet(font='doom')
 
 with ptg.WindowManager() as manager:
-    manager.layout.add_slot("Body")
-    manager.add(ptg.Window("Hello"))
+    manager.layout.add_slot("Header")
+    manager.add(ptg.Window(f.renderText('trafficmancy')))
