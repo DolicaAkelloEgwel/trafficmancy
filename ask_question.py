d7d2bef3a85112c25176609413fb0fd9e8f6e273
Dolica Akello-Egwel <d.akelloegwel@lcc.arts.ac.uk>
2024-09-17 11:17:03 +0100

    got the docker thing figured out

--- ask_question.py
+++ ask_question.py
@@ -1,8 +1,8 @@
-import ollama
+import requests
 
 TRAFFICMANCY_INITIAL_PROMPT = (
     "You are Trafficmancy. You harness the elements of the urban environment to provide answers to life's burning questions. "
-    "By analyzing the number of cars, buses, motorbikes, cyclists, and pedestrians detected in a single snapshot of the city, "
+    "By analyzing the number of cars, buses, motorbikes, cyclists, and pedestrians that moved during a 10-second observation of a nearby street, "
     "you interpret the scene to deliver guidance. Here's how you work:\n\n"
     "1. Cars: Represent stability and progress. A higher count of cars means the path ahead is clear, suggesting forward "
     "momentum and determination in your answer.\n\n"
@@ -18,15 +18,23 @@ TRAFFICMANCY_INITIAL_PROMPT = (
     "Whether it's a clear 'yes,' a thoughtful 'no,' or something in between, you guide the way based on the snapshot of the city in that moment."
 )
 
+# API URL pointing to localhost since the container is using host networking
+OLLAMA_API_URL = "http://localhost:11434/api/generate"
 
-def ask_question(model_to_use: str, query: str, counts: dict[str, int]) -> str:
-    response = ollama.chat(
-        model=model_to_use,
-        messages=[
-            {
-                "role": "user",
-                "content": f'{TRAFFICMANCY_INITIAL_PROMPT}. {counts["car"]} cars, {counts["person"]} pedestrians, {counts["bus"]} buses, {counts["motorbike"]} motorbikes, and {counts["bicycle"]} cyclists were observed. Based on this snapshot, analyse the observed elements and provide a symbolic interpretation that answers the following query: {query}',
-            },
-        ],
-    )
-    return response["message"]["content"]
+# Example request data
+data = {
+    "model": "dolphin-phi",
+    "prompt": "",
+    "stream": False,
+}
+
+
+def ask_question(query: str, counts: dict[str, int]) -> str:
+    query = f'{TRAFFICMANCY_INITIAL_PROMPT}. {counts["car"]} cars, {counts["person"]} pedestrians, {counts["bus"]} buses, {counts["motorbike"]} motorbikes, and {counts["bicycle"]} cyclists were observed. Based on this snapshot, analyse the observed elements and provide a symbolic interpretation that answers the following query: {query}'
+    data["prompt"] = query
+    response = requests.post(OLLAMA_API_URL, json=data)
+    if response.status_code == 200:
+        result = response.json()
+        return result["response"]
+    else:
+        return f"Failed to get a response: {response.status_code}"
