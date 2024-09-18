01e625eff8a3a16748554ea196593038b6517d24
Dolica Akello-Egwel <d.akelloegwel@lcc.arts.ac.uk>
2024-09-18 17:02:04 +0100

    instructions box @ bottom

--- pyxel-app.py
+++ pyxel-app.py
@@ -1,6 +1,7 @@
 import pyxel
-from traffic_counter import get_traffic_count
+
 from ask_question import ask_question
+from traffic_counter import get_traffic_count
 
 APP_WIDTH = 256 * 2
 APP_HEIGHT = 144 * 2
@@ -8,11 +9,11 @@ PADDING = 20
 
 BOX_WIDTH = APP_WIDTH - PADDING * 2
 
-PROCESSING_TEXT = "Processing"
-
 KEY_MAP = {}
 
 CHARACTER_LIMIT = 117
+
+INSTRUCTIONS = "Submit Question: Enter | Clear: Alt + C (Note - You may have to wait a while, as Trafficmany thinks about your query...)"
 
 
 def _split_up_long_text(output: str) -> str:
@@ -126,7 +127,6 @@ class App:
 
         # Check for submit button click
         if pyxel.btnp(pyxel.KEY_RETURN):
-            self.output_text = PROCESSING_TEXT + "..."
             traffic_count = get_traffic_count()
             self.output_text = ask_question(self.input_text, traffic_count)
             self.output_text = _split_up_long_text(self.output_text)
@@ -151,14 +151,22 @@ class App:
         pyxel.text(PADDING + 2, 82, self.input_text, 7)  # Display the input text
 
         # Create output box
-        pyxel.rect(PADDING - 1, 110 - 1, BOX_WIDTH + 2, 152, 8)
-        pyxel.rect(PADDING, 110, BOX_WIDTH, 150, 0)  # Black rectangle for output box
-        pyxel.text(PADDING + 2, 112, self.partial_text, 7)  # Display user output text
+        pyxel.rect(
+            PADDING - 1, 100 - 1, BOX_WIDTH + 2, 162, 8
+        )  # Red border for output box
+        pyxel.rect(PADDING, 100, BOX_WIDTH, 160, 0)  # Black rectangle for output box
+        pyxel.text(PADDING + 2, 102, self.partial_text, 7)  # Display user output text
 
         self.partial_text = self.output_text[: self.progress]
 
         if self.progress < self.end:
             self.progress += 1
+
+        pyxel.rect(0, APP_HEIGHT - 11, APP_WIDTH, 12, 8)  # Red line for
+        pyxel.rect(
+            0, APP_HEIGHT - 10, APP_WIDTH, 10, 0
+        )  # Black rectangle for output box
+        pyxel.text(2, APP_HEIGHT - 8, INSTRUCTIONS, 8)
 
 
 # Run the application
