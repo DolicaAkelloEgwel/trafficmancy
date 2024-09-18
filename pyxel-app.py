f58465adec787b7bea105f6417145f89f52bbde7
Dolica Akello-Egwel <d.akelloegwel@lcc.arts.ac.uk>
2024-09-18 14:01:49 +0100

    making output appear one character at a time

--- pyxel-app.py
+++ pyxel-app.py
@@ -13,13 +13,15 @@ PROCESSING_TEXT = "Processing"
 KEY_MAP = {}
 
 
-
 class App:
     def __init__(self):
         pyxel.init(APP_WIDTH, APP_HEIGHT, title="Trafficmancy")
         pyxel.load("background.pyxres")
         self.input_text = ""
-        self.output_text = ""
+        self.output_text = " "
+        self.partial_text = ""
+        self.progress = 1
+        self.end = 1
         self.processing_query = False  # Track if typing in input box
         self.count = 0
         pyxel.run(self.update, self.draw)
@@ -29,7 +31,9 @@ class App:
         if not self.processing_query:
             for i in range(26):
                 if pyxel.btnp(pyxel.KEY_A + i):
-                    if pyxel.btn(pyxel.KEY_SHIFT):  # Check if shift is pressed for uppercase
+                    if pyxel.btn(
+                        pyxel.KEY_SHIFT
+                    ):  # Check if shift is pressed for uppercase
                         self.input_text += chr(pyxel.KEY_A + i).upper()
                     else:
                         self.input_text += chr(pyxel.KEY_A + i)
@@ -41,50 +45,50 @@ class App:
 
             # Handle punctuation symbols using their respective Pyxel key codes
             if pyxel.btnp(pyxel.KEY_SPACE):
-                self.input_text += ' '
+                self.input_text += " "
             if pyxel.btnp(pyxel.KEY_COMMA):
-                self.input_text += ','
+                self.input_text += ","
             if pyxel.btnp(pyxel.KEY_PERIOD):
-                self.input_text += '.'
+                self.input_text += "."
             if pyxel.btnp(pyxel.KEY_SLASH):
-                self.input_text += '/'
+                self.input_text += "/"
             if pyxel.btnp(pyxel.KEY_SEMICOLON):
-                self.input_text += ';'
+                self.input_text += ";"
             if pyxel.btnp(pyxel.KEY_MINUS):
-                self.input_text += '-'
+                self.input_text += "-"
             if pyxel.btnp(pyxel.KEY_EQUALS):
-                self.input_text += '='
+                self.input_text += "="
             if pyxel.btnp(pyxel.KEY_BACKSLASH):
-                self.input_text += '\\'
+                self.input_text += "\\"
 
             # Handle shift-modified symbols for special characters like ?, !, etc.
             if pyxel.btn(pyxel.KEY_SHIFT):
                 if pyxel.btnp(pyxel.KEY_1):
-                    self.input_text += '!'
+                    self.input_text += "!"
                 if pyxel.btnp(pyxel.KEY_2):
-                    self.input_text += '@'
+                    self.input_text += "@"
                 if pyxel.btnp(pyxel.KEY_3):
-                    self.input_text += '#'
+                    self.input_text += "#"
                 if pyxel.btnp(pyxel.KEY_4):
-                    self.input_text += '$'
+                    self.input_text += "$"
                 if pyxel.btnp(pyxel.KEY_5):
-                    self.input_text += '%'
+                    self.input_text += "%"
                 if pyxel.btnp(pyxel.KEY_6):
-                    self.input_text += '^'
+                    self.input_text += "^"
                 if pyxel.btnp(pyxel.KEY_7):
-                    self.input_text += '&'
+                    self.input_text += "&"
                 if pyxel.btnp(pyxel.KEY_8):
-                    self.input_text += '*'
+                    self.input_text += "*"
                 if pyxel.btnp(pyxel.KEY_9):
-                    self.input_text += '('
+                    self.input_text += "("
                 if pyxel.btnp(pyxel.KEY_0):
-                    self.input_text += ')'
+                    self.input_text += ")"
                 if pyxel.btnp(pyxel.KEY_COMMA):
-                    self.input_text += '<'
+                    self.input_text += "<"
                 if pyxel.btnp(pyxel.KEY_PERIOD):
-                    self.input_text += '>'
+                    self.input_text += ">"
                 if pyxel.btnp(pyxel.KEY_SLASH):
-                    self.input_text += '?'
+                    self.input_text += "?"
 
         # Handle backspace to remove last character
         if pyxel.btnp(pyxel.KEY_BACKSPACE) and self.input_text:
@@ -96,6 +100,10 @@ class App:
             traffic_count = get_traffic_count()
             self.output_text = ask_question(self.input_text, traffic_count)
 
+            print(self.output_text)
+            self.end = len(self.output_text)
+            self.progress = 1
+
     def draw(self):
         pyxel.cls(7)  # Clear screen to white
 
@@ -106,12 +114,21 @@ class App:
         pyxel.blt(APP_WIDTH // 2, APP_HEIGHT // 2, 0, 0, 0, APP_WIDTH, APP_HEIGHT)
 
         # Create input box
+        pyxel.rect(
+            PADDING - 1, 80 - 1, BOX_WIDTH + 2, 12, 8
+        )  # Red border for input box
         pyxel.rect(PADDING, 80, BOX_WIDTH, 10, 0)  # Black rectangle for input
         pyxel.text(PADDING + 2, 82, self.input_text, 7)  # Display the input text
 
         # Create output box
+        pyxel.rect(PADDING - 1, 110 - 1, BOX_WIDTH + 2, 152, 8)
         pyxel.rect(PADDING, 110, BOX_WIDTH, 150, 0)  # Black rectangle for output box
-        pyxel.text(PADDING + 2, 115, self.output_text, 7)  # Display user output text
+        pyxel.text(PADDING + 2, 112, self.partial_text, 7)  # Display user output text
+
+        self.partial_text = self.output_text[: self.progress]
+
+        if self.progress < self.end:
+            self.progress += 1
 
 
 # Run the application
