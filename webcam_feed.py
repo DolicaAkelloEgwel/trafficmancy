74a632c0b49d183cf90d04890c456ae656f39262
DolicaAkelloEgwel <d.egwel@gmail.com>
2025-03-11 16:44:56 +0000

    split up downloading code

--- webcam_feed.py
+++ webcam_feed.py
@@ -1,22 +1,9 @@
-import subprocess
+import blobconverter
+import cv2
+import numpy as np
+import time
+from tracking import TrackableObject
 
-# Live stream URL
-YOUTUBE_URL = "https://www.youtube.com/watch?v=jzgnL2FkrXc"
 
-# Output filename
-OUTPUT_FILE = "latest.mp4"
-
-# yt-dlp command to grab the latest 10 seconds of a live stream
-command = [
-    "yt-dlp",
-    "--download-sections",
-    "*-10",  # Download the most recent 10 seconds
-    "-o",
-    OUTPUT_FILE,  # Output file name
-    YOUTUBE_URL,
-]
-
-# Run command
-subprocess.run(command)
-
-print(f"Downloaded latest 10s clip: {OUTPUT_FILE}")
+def get_traffic_count():
+    return None
