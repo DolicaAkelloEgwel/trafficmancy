49004df2a7c64c52c8b2d14e848a300e2e81b882
DolicaAkelloEgwel <d.egwel@gmail.com>
2025-03-14 17:43:15 +0000

    showing disruptions

--- tfl_wrapper.py
+++ tfl_wrapper.py
@@ -4,4 +4,5 @@ with open("app.key", "r") as f:
     app_key = f.readline()
 
 disruptions = tflwrapper.disruptions(app_key)
-print(disruptions.getAllLifts())
+for lift in disruptions.getAllLifts():
+    print(lift)
