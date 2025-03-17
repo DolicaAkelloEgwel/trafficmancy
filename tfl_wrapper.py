ef8a9e156cdd6df8eae10b4174bb6d8d4b0155ae
DolicaAkelloEgwel <d.egwel@gmail.com>
2025-03-17 17:21:12 +0000

    all the stratford naptans

--- tfl_wrapper.py
+++ tfl_wrapper.py
@@ -1,8 +1,22 @@
 import tflwrapper
 
+STRATFORD_NAPTANS = ["9400ZZLUSTD","9400ZZLUSTD1","9400ZZLUSTD2","9400ZZLUSTD3","9400ZZLUSTD4","9400ZZLUSTD5","9400ZZLUSTD6"]
+
 with open("app.key", "r") as f:
     app_key = f.readline()
 
 disruptions = tflwrapper.disruptions(app_key)
 for lift in disruptions.getAllLifts():
     print(lift)
+
+# print(tflwrapper.line.getArrivalsByNaptan(lineids=["central", "dlr", "elizabeth", "jubilee", "mildmay"], naptan="940GZZLUSTD", destinationStationId=""))
+
+crowding = tflwrapper.crowding(app_key)
+
+for naptan in STRATFORD_NAPTANS:
+    for crowd in crowding.getAllByNaptan(naptan):
+        print(crowd)
+
+line = tflwrapper.line(app_key)
+
+print(line.getTimetableFromStation(_line = "central", NaPTANID=STRATFORD_NAPTANS[0], direction="outbound"))
