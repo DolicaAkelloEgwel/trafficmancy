4d1d0010b2f2e3886f6a4de02b8aad16debd2d59
DolicaAkelloEgwel <d.egwel@gmail.com>
2025-03-24 11:18:13 +0000

    trying to get tfl info with this damn wrapper...

--- tfl_wrapper.py
+++ tfl_wrapper.py
@@ -1,15 +1,20 @@
 import tflwrapper
 
-STRATFORD_NAPTANS = ["9400ZZLUSTD","9400ZZLUSTD1","9400ZZLUSTD2","9400ZZLUSTD3","9400ZZLUSTD4","9400ZZLUSTD5","9400ZZLUSTD6"]
+STRATFORD_NAPTANS = [
+    "9400ZZLUSTD",
+    "9400ZZLUSTD1",
+    "9400ZZLUSTD2",
+    "9400ZZLUSTD3",
+    "9400ZZLUSTD4",
+    "9400ZZLUSTD5",
+    "9400ZZLUSTD6",
+]
+STRATFORD_BIKE_POINT_ID = "BikePoints_790"
 
 with open("app.key", "r") as f:
     app_key = f.readline()
 
-disruptions = tflwrapper.disruptions(app_key)
-for lift in disruptions.getAllLifts():
-    print(lift)
-
-# print(tflwrapper.line.getArrivalsByNaptan(lineids=["central", "dlr", "elizabeth", "jubilee", "mildmay"], naptan="940GZZLUSTD", destinationStationId=""))
+bike_point = tflwrapper.bikePoint(app_key)
 
 crowding = tflwrapper.crowding(app_key)
 
@@ -19,7 +24,23 @@ for naptan in STRATFORD_NAPTANS:
 
 line = tflwrapper.line(app_key)
 
-print(line.getTimetableFromStation(_line = "central", NaPTANID=STRATFORD_NAPTANS[0], direction="outbound"))
+for naptan in STRATFORD_NAPTANS:
+    print(
+        line.getTimetableFromStation(
+            _line="central", NaPTANID=naptan, direction="outbound"
+        )
+    )
+
 
 def get_tfl_data():
-    pass
+    data = {}
+
+    disruptions = tflwrapper.disruptions(app_key)
+    data["num-broken-lifts"] = len(disruptions.getAllLifts())
+
+    air_quality = tflwrapper.airQuality(app_key)
+    data["air-quality"] = air_quality.getAirQuality()["currentForecast"][0][
+        "forecastSummary"
+    ]
+
+    return data
