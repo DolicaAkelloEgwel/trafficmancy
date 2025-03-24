f2019b6928e1d024e5d4b3654b259c9542b6b7db
DolicaAkelloEgwel <d.egwel@gmail.com>
2025-03-24 15:40:16 +0000

    function for getting arrival info

--- tfl_wrapper.py
+++ tfl_wrapper.py
@@ -1,30 +1,43 @@
 import tflwrapper
 
-STRATFORD_NAPTANS = [
-    "9400ZZLUSTD",
-    "9400ZZLUSTD1",
-    "9400ZZLUSTD2",
-    "9400ZZLUSTD3",
-    "9400ZZLUSTD4",
-    "9400ZZLUSTD5",
-    "9400ZZLUSTD6",
-]
 STRATFORD_BIKE_POINT_ID = "BikePoints_790"
 STRATFORD_LINES = ["elizabeth-line", "dlr", "tube"]
-STATUS_NAMES = ["elizabeth", "dlr", "central", "jubilee"]
+STATUS_NAMES = ["elizabeth", "dlr", "central", "mildmay", "jubilee"]
+STRATFORD_NAPTANS = {name: None for name in STATUS_NAMES}
+
+
+def _is_stratford_tube_station(name: str) -> bool:
+    return (
+        "Stratford" in name
+        and "High Street" not in name
+        and "International" not in name
+    )
+
 
 with open("app.key", "r") as f:
     app_key = f.readline()
 
 line = tflwrapper.line(app_key)
 
-statuses = line.getStatusByID(STRATFORD_LINES + ["tube"], True)
-status_info = ""
-for status in statuses:
-    if status["id"] in STATUS_NAMES:
-        status_info += f" {status['name']} has {len(status['disruptions'])} disruptions and has {status['lineStatuses'][0]['statusSeverityDescription']}."
+# Get the Naptan IDs for Stratford Station
+for name in STATUS_NAMES:
+    stop_points = line.getAllStopPoints(name)
+    for stop_point in stop_points:
+        if _is_stratford_tube_station(stop_point["commonName"]):
+            STRATFORD_NAPTANS[name] = stop_point["naptanId"]
 
-status_info = status_info[1:]
+
+def _get_arrival_with_naptan(line_name: str, naptan: str):
+    if line_name in ["mildmay", "jubilee"]:
+        return line.getArrivalsByNaptan(
+            [line_name], STRATFORD_NAPTANS[line_name], STRATFORD_NAPTANS[line_name]
+        )
+    else:
+        return []
+
+
+for line_name in STRATFORD_NAPTANS:
+    print(_get_arrival_with_naptan(line_name, STRATFORD_NAPTANS[line_name])[:5])
 
 
 def get_tfl_data():
