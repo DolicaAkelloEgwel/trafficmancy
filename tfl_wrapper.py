29bf25de447397ddb75651d6ff8f9f03a8663007
Dolica Akello-Egwel <d.akelloegwel@lcc.arts.ac.uk>
2025-03-25 12:18:58 +0000

    some more refactoring

--- tfl_wrapper.py
+++ tfl_wrapper.py
@@ -35,27 +35,61 @@ MILDMAY = "mildmay"
 JUBILEE = "jubilee"
 DLR = "dlr"
 
+# ID of the bike point near Stratford Station
 STRATFORD_BIKE_POINT_ID = "BikePoints_790"
+
+# API has some confusing thing where "elizabeth" is used in some places and "elizabeth-line" is used in others but "dlr" is consistent...
 STRATFORD_LINES = ["elizabeth-line", DLR, "tube"]
+
+# List of the lines that pass through Stratford station
 LINES_THAT_GO_THROUGH_STRATFORD = [ELIZABETH, DLR, CENTRAL, MILDMAY, JUBILEE]
+
+# Get the different Naptans for Stratford station
 STRATFORD_NAPTANS = {line_name: None for line_name in LINES_THAT_GO_THROUGH_STRATFORD}
-DEST_NAPTANS = {line_name: [] for line_name in LINES_THAT_GO_THROUGH_STRATFORD}
+
+# Dictionary for the termini Naptans for all the different lines that pass through Stratford
+TERMINI_NAPTANS = {line_name: [] for line_name in LINES_THAT_GO_THROUGH_STRATFORD}
 
 
 def _is_stratford_tube_station(name: str) -> bool:
-    """ """
+    """Checks if the station name is actually for Stratford and not Stratford International or Stratford High Street
+
+    Args:
+        name (str): The station name.
+
+    Returns:
+        bool: True if 'Stratford' is in the name, and 'High Street' and 'International' are not in the name.
+    """
     return (
         STRATFORD in name and "High Street" not in name and "International" not in name
     )
 
 
-def _find_station_naptan_on_line(line_name: str, station_name: str):
-    """ """
+def _find_station_naptan_on_line(line_name: str, station_name: str) -> StationNaptan:
+    """Finds a station naptan code given a line name and a station name.
+
+    Args:
+        line_name (str): The line name.
+        station_name (str): The station name.
+
+    Returns:
+        StationNaptan: A StationNaptan tuple.
+    """
     stop_points = line.getAllStopPoints(line_name)
     naptans = []
-    for stop_point in stop_points:
-        if station_name in stop_point["commonName"]:
-            naptans.append((station_name, stop_point["naptanId"]))  # todo - named tuple
+    if station_name is not STRATFORD:
+        for stop_point in stop_points:
+            if station_name in stop_point["commonName"]:
+                naptans.append(
+                    (station_name, stop_point["naptanId"])
+                )  # todo - named tuple
+    else:
+        for stop_point in stop_points:
+            if _is_stratford_tube_station(stop_point["commonName"]):
+                naptans.append(
+                    (station_name, stop_point["naptanId"])
+                )  # todo - named tuple
+    print(naptans)
     return naptans
 
 
@@ -67,36 +101,35 @@ line = tflwrapper.line(app_key)
 # Get the Naptan IDs for Stratford Station on the different lines
 for line_name in LINES_THAT_GO_THROUGH_STRATFORD:
     naptans = _find_station_naptan_on_line(line_name, STRATFORD)
-    naptans = [naptan for naptan in naptans if _is_stratford_tube_station(naptan[0])][0]
-    STRATFORD_NAPTANS[line_name] = naptans[1]
+    STRATFORD_NAPTANS[line_name] = naptans[0][1]
 
 # Mildmay and Jubilee terminate at Stratford, so just ask for trains that have Stratford as its destination
-DEST_NAPTANS[MILDMAY].append(STRATFORD_NAPTANS[MILDMAY])
-DEST_NAPTANS[JUBILEE].append(STRATFORD_NAPTANS[JUBILEE])
+TERMINI_NAPTANS[MILDMAY].append(STRATFORD_NAPTANS[MILDMAY])
+TERMINI_NAPTANS[JUBILEE].append(STRATFORD_NAPTANS[JUBILEE])
 
 # Get Naptans for Central line termini
-DEST_NAPTANS[CENTRAL] = [
+TERMINI_NAPTANS[CENTRAL] = [
     _find_station_naptan_on_line(CENTRAL, terminus)[0][1]
     for terminus in CENTRAL_TERMINI
 ]
 # Naptans for Elizabeth line termini
-DEST_NAPTANS[ELIZABETH] = [
+TERMINI_NAPTANS[ELIZABETH] = [
     _find_station_naptan_on_line(ELIZABETH, terminus)[0][1]
     for terminus in ELIZABETH_TERMINI
 ]
 # Naptans for DLR termini
-DEST_NAPTANS[DLR] = [
+TERMINI_NAPTANS[DLR] = [
     _find_station_naptan_on_line(DLR, terminus)[0][1] for terminus in DLR_TERMINI
 ]
 
 # Find info on trains heading to/from Stratford
-for line_name in DEST_NAPTANS:
-    for dest_naptan in DEST_NAPTANS[line_name]:
-        print(dest_naptan)
+for line_name in TERMINI_NAPTANS:
+    for terminus_naptan in TERMINI_NAPTANS[line_name]:
         arrivals = line.getArrivalsByNaptan(
-            [line_name], STRATFORD_NAPTANS[line_name], dest_naptan
+            [line_name], STRATFORD_NAPTANS[line_name], terminus_naptan
         )[:1]
-        print(arrivals)
+        if not arrivals:
+            print(line_name, terminus_naptan)
 
 
 def get_tfl_data():
